c2072b2d06be3cd3c1833bd22fd7e09682d8ea7a
ByteNext Technologies <99094343+bytenexttech@users.noreply.github.com>
2023-06-30 14:46:51 +0530

    Add files via upload

--- src/components/Refund.js
+++ src/components/Refund.js
@@ -1,10 +1,21 @@
 import '../components/css/Terms.css'
 function Refund() {
     return (
-      <div className='Refund-Page'>
-        
-      </div>
+        <div className='Refund-Page'>
+            <h2>RETURN POLICY</h2>
+
+            <h3>REFUNDS</h3>
+
+            <p>All sales are final and no refund will be issued.</p>
+
+            <h3>QUESTIONS</h3>
+
+            <p>If you have any questions concerning our return policy, please contact us at:</p>
+
+            <a href='mailto:hello@bytenexttechnologies.in'>hello@bytenexttechnologies.in</a>
+            <br /><br />
+        </div>
     );
-  }
-  
-  export default Refund;
+}
+
+export default Refund;
